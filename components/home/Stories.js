514c1dccf087766cd8345051ac9257d961eaba19
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-23 00:59:55 +0300

    Create stories component

---
+++ components/home/Stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, ScrollView, Image } from 'react-native';
+
+const Stories = () => {
+    return (
+        <View style={{ marginBottom: 13 }}>
+            <ScrollView
+                horizontal
+                showsHorizontalScrollIndicator={false}
+            >
+            </ScrollView>
+        </View>
+    );
+};
+
+export default Stories;
