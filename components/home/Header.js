269b8bff12fad386beb96608beb59aa67a652dc2
dbegogow <60520477+dbegogow@users.noreply.github.com>
2021-10-23 00:40:09 +0300

    Implement header component

--- components/home/Header.js
+++ components/home/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 const Header = () => {
     return (
@@ -7,6 +7,36 @@ const Header = () => {
             <TouchableOpacity>
                 <Text style={styles.title}>SMedia</Text>
             </TouchableOpacity>
+
+            <View style={styles.iconsContainer}>
+                <TouchableOpacity>
+                    <Image
+                        source={{
+                            uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/plus-2-math.png'
+                        }}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Image
+                        source={{
+                            uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/like--v1.png'
+                        }}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <View style={styles.unreadBadge}>
+                        <Text style={styles.unreadBadgeText}>11</Text>
+                    </View>
+                    <Image
+                        source={{
+                            uri: 'https://img.icons8.com/fluency-systems-regular/60/ffffff/facebook-messenger.png'
+                        }}
+                        style={styles.icon}
+                    />
+                </TouchableOpacity>
+            </View>
         </View>
     );
 };
@@ -17,13 +47,36 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flexDirection: 'row',
         marginHorizontal: 20,
-        marginTop: 18
+        marginTop: 13
+    },
+    iconsContainer: {
+        flexDirection: 'row'
     },
     title: {
         color: 'white',
-        width: 100,
-        height: 50,
         fontSize: 25
+    },
+    icon: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
+        resizeMode: 'contain'
+    },
+    unreadBadge: {
+        backgroundColor: '#FF0000',
+        position: 'absolute',
+        left: 20,
+        bottom: 18,
+        width: 20,
+        height: 20,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        zIndex: 3
+    },
+    unreadBadgeText: {
+        color: 'white',
+        fontWeight: '600'
     }
 });
 
